00187c26d21509d0985f034270926e9c81678650
Wismal <albertowisma@gmail.com>
2025-06-07 01:22:12 -0400

    Project 3 done, dio mio

--- class-3/app.js
+++ class-3/app.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
-const { dir } = require('node:console')
+const cors = require('cors') // Import the CORS middleware
+// Import the validation functions from the schema file
+const { validateMovie, validatePartialMovie } = require('./schema/validateMovies')
+
 const app = express()
 app.use(express.json()) // Middleware to parse JSON request bodies
+app.use(cors()) // Enable CORS for all routes
 app.disable('x-powered-by') // Disable the 'X-Powered-By' header
+// Add your frontend URL here
 
 app.get('/movies', (req, res) => {
   const { genre } = req.query // req it is an object that contains the query parameters
-
   /*
   only works if the user types the genre correctly in the URL (not case-sensitive)
 if (genre) {
@@ -39,32 +43,48 @@ app.get('/movies/:id', (req, res) => {
   }
 })
 
-// We use always the same resource to create a new movie (API REST). No /crate-movie or /add-movie
 app.post('/movies', (req, res) => {
-  const {
-    title,
-    year,
-    genre,
-    director,
-    duration,
-    poster,
-    rate
-  } = req.body
-
+  const result = validateMovie(req.body)
+  if (result.error) {
+    return res.status(400).json({ error: result.error.message })
+  }
   const newMovie = {
     id: crypto.randomUUID(), // Generate a unique ID for the new movie. Universal Unique Identifier (UUID)
-    title,
-    year,
-    genre,
-    director,
-    duration,
-    poster,
-    rate: rate ?? 0
+    ...result.data // Use the validated data from the request body
   }
 
   movies.push()// This in not REST, because we are not using a database (we are saving the data in memory)
 
   res.status(201).json(newMovie) // Return the created movie with a 201 status code
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const result = validatePartialMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: result.error.message })
+  }
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+  movies[movieIndex] = updateMovie // Update the movie in the array
+  return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+  movies.splice(movieIndex, 1) // Remove the movie from the array
+  res.json({ message: 'Movie deleted successfully' }) // Return a success message
 })
 
 app.listen(1234, () => {
---
+++ class-3/schema/validateMovies.js
@@ -0,0 +1,33 @@
+const z = require('zod')
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  duration: z.number().int().min(1).max(600),
+  rate: z.number().min(0).max(10).optional({
+    message: 'Rate must be a number between 0 and 10'
+  }),
+  poster: z.string().url(),
+  genre: z.array(
+    z.enum(['Action', 'Comedy', 'Drama', 'Horror', 'Sci-Fi', 'Romance']),
+    {
+      required_error: 'Movie genre is required'
+    }
+  )
+
+})
+// safeParse in a zod method that returns an object with success and error properties
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+// This zod methos allows partial validation, meaning that not all fields are required
+
+function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = { validateMovie, validatePartialMovie }
